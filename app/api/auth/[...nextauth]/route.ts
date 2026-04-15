8af70111a018b793699e2c5089c3a85eb5b3027b
John Uguru <johnuguru73@gmail.com>
2026-04-15 08:41:51 +0100

    chore: cloudflare deployment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,6 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "@/components/auth/config/auth-options";
 
-export const runtime = "edge";
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
