d2f2c683e48b8c0b9d2398262b121c9fa71a4586
John Uguru <johnuguru73@gmail.com>
2026-04-15 08:21:02 +0100

    chore: cloudflare deployment fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 import { RootProviders } from "@/components/providers/root-providers"
 import { cn } from "@/lib/utils";
 
+export const runtime = "edge"
+
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
